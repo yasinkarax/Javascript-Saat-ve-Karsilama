53eced2d0db88e82ae11c8890fd4546f8989d464
Yasin KARA <422884@ogr.ktu.edu.tr>
2022-05-15 01:27:46 +0300

    kullanıcıdan isim alındı, gün yazdırılmak için günler dizisi oluşturuldu

---
+++ main.js
@@ -0,0 +1,12 @@
+let isim = prompt("isminiz: ");
+document.getElementById('name').innerHTML = isim;
+let days = [
+    "pazar",
+    "salı",
+    "çarşamba",
+    "perşembe",
+    "cuma",
+    "cumartesi",
+    "pazartesi"
+];
+
