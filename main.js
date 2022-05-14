ffe069769a4e3e2477cc96958b8d52d0bc900e8d
Yasin KARA <422884@ogr.ktu.edu.tr>
2022-05-15 01:28:29 +0300

    tarihleri alan ve ekrana yazdıran fonksiyon yazıldı

--- main.js
+++ main.js
@@ -10,3 +10,18 @@ let days = [
     "pazartesi"
 ];
 
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    let day = today.getDay();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('date').innerHTML =  h + ":" + m + ":" + s + " " + days[day];
+    setTimeout(startTime, 1000);
+  }
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
